71b7894622c6003e078f7b911088e888f1bc38b0
Godwin Martins <gmocdes20@gmail.com>
2021-04-17 15:55:30 +0100

    Initial commit, just done the home pagee

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var navLinks = document.getElementById("nav-links");
+var open = document.getElementsByClassName('close-menu')
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-100%";
+  // open.style.display = 'none'
+}
